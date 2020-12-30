44f2e839bac1dd907727c7744aac50054051a32f
Kamil Kaczowka <s21790@pjwstk.edu.pl>
2020-12-30 16:10:18 +0100

    add AspectJ Jar, enable AspectJ auto proxy and create Aspect

---
+++ src/com/kamkacz/springdemo/aspect/CRMLoggingAspect.java
@@ -0,0 +1,20 @@
+package com.kamkacz.springdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class CRMLoggingAspect {
+
+    // setup logger
+    private Logger myLogger = Logger.getLogger(getClass().getName());
+
+    // setup pointcut declarations
+
+    // add @Before advice
+
+    // add @AfterReturning advice
+}
